5f8277442248d45a40307c48c0b806be1d93a189
ခွန်ဖန်ဒွဲ့ <127810188+KhunPhanDuae@users.noreply.github.com>
2025-01-04 20:35:20 +0630

    Update TR.js

--- H/TR.js
+++ H/TR.js
@@ -4,7 +4,11 @@
             // Static translation logic
             if (englishText === "Hello") {
                 document.getElementById("PaOOutput").value = "ခွဲးဟဝ်ဒျာႏ";
-            } else if (englishText === "How are you?") {
+           } else if (englishText === "place") {
+    document.getElementById("PaOOutput").value = "ခင်ႏလမ်း";
+} else if (englishText === "warm") {
+    document.getElementById("PaOOutput").value = "လွံꩻ";
+ } else if (englishText === "How are you?") {
                 document.getElementById("PaOOutput").value = "အုံဟဝ်နေꩻ?";
             } else {
                 document.getElementById("PaOOutput").value = "Translation not available.";
